3352a93a0bdb02176fb601a427fed8d073ac41c8
Adrian Roldán <adrianaer7@hotmail.com>
2022-08-10 15:34:36 -0300

    SD

--- admin/index.php
+++ admin/index.php
@@ -1,14 +1,56 @@
 <?php 
+    //Importar template
     require "../includes/funciones.php";
-
     incluirTemplate("header", $pagina = "Admin - Inicio");
+
+    //Importar la conexion
+    require "../includes/config/database.php";
+    $db = conectarDB();
+    //Escribir el Query
+    $query = "SELECT * FROM propiedades";
+    //Consultar a la BD
+    $resultado = mysqli_query($db, $query);
+
+    exit;
+    $resultado = $_GET["resultado"] ?? null;    //busca el resultado en la URL y si no existe le asigno null
+    
 ?>
 
 
 
     <main class="contenedor seccion">
         <h1>Administrador de bienes raices</h1>
+        <?php if(intval($resultado) === 1) { ?> <!--intval convierte el valor a numero-->
+            <p class="alerta exito">Propiedad cargada correctamente</p>
+        <?php } ?>
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva propiedad</a>     
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titulo</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la playa</td>
+                    <td><img src="/imagenes/archivo.jpg" class="imagen-tabla"></td>
+                    <td>$120000</td>
+                    <td>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <a href="#" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 
-<?php incluirTemplate("footer");?>
+<?php 
+    //Cerrar la conexion
+
+    incluirTemplate("footer");
+?>
--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -3,6 +3,10 @@
     require "../../includes/config/database.php";
     $db = conectarDB();
 
+    //Consultar para obtener los vendedores
+    $consulta = "SELECT * FROM vendedores";
+    $resultado = mysqli_query($db, $consulta);
+
     //Arreglo con mensaje de error
     $errores = [];
 
@@ -17,14 +21,17 @@
     //Ejecutar el codigo despues que el usuario envia el codigo
     if($_SERVER["REQUEST_METHOD"] === "POST") {
         
-
-        $titulo = $_POST["titulo"];
-        $precio = $_POST["precio"];
-        $descripcion = $_POST["descripcion"];
-        $habitaciones = $_POST["habitaciones"];
-        $wc = $_POST["wc"];
-        $estacionamiento = $_POST["estacionamiento"];
-        $vendedores_id = $_POST["vendedor"];
+        $titulo = mysqli_escape_string($db, $_POST["titulo"]);  //mysqli_escape_string sanitiza el campo, en caso que injecten una sentencia de sql o scripting, guarda eso como string y no como ejecutable en la bd 
+        $precio = mysqli_escape_string($db, $_POST["precio"]);
+        $descripcion = mysqli_escape_string($db, $_POST["descripcion"]);
+        $habitaciones = mysqli_escape_string($db, $_POST["habitaciones"]);
+        $wc = mysqli_escape_string($db, $_POST["wc"]);
+        $estacionamiento = mysqli_escape_string($db, $_POST["estacionamiento"]);
+        $vendedores_id = mysqli_escape_string($db, $_POST["vendedor"]);
+        $creado = date("Y/m/d");
+        
+        //Asignar files hacia una variable. Los archivos no se leen por $_POST, sino por $_FILES
+        $imagen = $_FILES["imagen"];    //accedo al name del input
 
         if(!$titulo) {
             $errores[] = "El titulo es obligatorio";
@@ -47,20 +54,40 @@
         if(!$vendedores_id) {
             $errores[] = "El nombre del vendedor es obligatorio";
         }
+        if(!$imagen["name"]) {
+            $errores[] = "La imagen es obligatoria";
+        }
+
+        //Validar imagen por tamaño
+        $medida = 1000 * 1000;   //bytes a kb (1Mb maximo)
+        if($imagen["size"] > $medida) {
+            $errores[] = "La imagen es muy pesada";
+        }
 
         //Revisar que el array de errores esté vacio
         if(empty($errores)) {
+            //Crear carpeta para subir archivo
+            $carpetaImagenes = "../../imagenes/";   //guardo la ubicacion donde quiero que se cree la carpeta
+            if(!is_dir($carpetaImagenes)) { //verifica si esa carpeta ya está creada
+                mkdir($carpetaImagenes);    //crea la carpeta
+            }
+            //Generar un nombre unico para cada archivo
+            $nombreImagen = md5(uniqid(rand(), true));  //mk5 devuelve un hash estatico. iniqid genera aleatorios
+            //Guardar archivo en carpeta
+            move_uploaded_file($imagen["tmp_name"], $carpetaImagenes . "${nombreImagen}.jpg");
+
+
             //Insertar en la bd
-            $query = "INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, wc, estacionamiento, vendedores_id) 
-                      VALUES ('$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$vendedores_id')";
+            $query = "INSERT INTO propiedades (titulo, precio, imagen, descripcion, habitaciones, wc, estacionamiento, creado, vendedores_id) 
+                      VALUES ('$titulo', '$precio', '$nombreImagen', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$creado', '$vendedores_id')";
     
-            $resultado = mysqli_query($db, $query);   
+            $resultado = mysqli_query($db, $query);
+
+            //Redireccionar
             if($resultado) {
-                echo "Insertado correctamente";
+                header("Location: /admin?resultado=1"); //no se puede usar en el html. Le paso el string resultado para que en el index se lo muestre
             }
         }
-
-
     }
 
     require "../../includes/funciones.php";
@@ -76,10 +103,13 @@
                 <?php echo $error; ?>
             </div>
         <?php } ?>
+
+        <!--El enctype del form permite que se suban archivos mediante el $_FILE-->
         <form 
             method="POST" 
             action="/admin/propiedades/crear.php"
             class="formulario" 
+            enctype="multipart/form-data"
         >
             <fieldset>
                 <legend>Informacion general</legend>
@@ -114,9 +144,7 @@
                 <textarea 
                     name="descripcion" 
                     id="descripcion"
-                >
-                    <?php echo $descripcion ?>
-                </textarea>
+                ><?php echo $descripcion; ?></textarea>
             </fieldset>
 
             <fieldset>
@@ -161,9 +189,15 @@
 
                 <select name="vendedor">
                     <option value="">--Seleccione--</option>
-                    <option value="1">Adrian</option>
-                    <option value="2">Eduardo</option>
-
+                    <?php while($vendedor = mysqli_fetch_assoc($resultado)) { ?>    <!--el assoc convierte la tabla obtenida en un array asociativo-->
+                        <!--al option le agrego la clase selected si ya envie el formulario y el id que envié es igual al que se encontró en la consulta a la bd-->
+                        <option 
+                            value="<?php echo $vendedor["id"];?>"
+                            <?php echo $vendedores_id === $vendedor["id"] ? "selected" : ""; ?> 
+                        >
+                            <?php echo $vendedor["nombre"]." ".$vendedor["apellido"]; ?>
+                        </option>
+                    <?php }; ?>
                 </select>
             </fieldset>
 
